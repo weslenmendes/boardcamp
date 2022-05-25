6c8bac54d35aad8fb537ec73a3c73f0d5c9e38bc
Weslen Mendes <weslenmends@gmail.com>
2022-05-25 12:28:30 -0300

    chore: added bank connection

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import express, { json } from "express";
 import cors from "cors";
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(json());
